0a45e9289fbde44f0a692315b31bc4b9f6aba5e8
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2013-04-24 15:15:48 +0200

    Added TaskListUpdateForm

--- task_list/forms.py
+++ task_list/forms.py
@@ -4,17 +4,28 @@ from django import forms
 from .models import TaskList
 
 
-class TaskListCreateForm(forms.ModelForm):
+class TaskListFormMixin(object):
+    """Mixin to add common methods to all TaskList forms."""
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        super(TaskListFormMixin, self).__init__(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        instance = super(TaskListFormMixin, self).save(*args, **kwargs)
+        if not self.user in instance.users.all():
+            instance.users.add(self.user)
+        return instance
+
+
+class TaskListCreateForm(TaskListFormMixin, forms.ModelForm):
     """ModelForm to create an instance of the ``TaskList`` model."""
     class Meta:
         model = TaskList
         fields = ('title',)
 
-    def __init__(self, user, *args, **kwargs):
-        self.user = user
-        super(TaskListCreateForm, self).__init__(*args, **kwargs)
 
-    def save(self, *args, **kwargs):
-        instance = super(TaskListCreateForm, self).save(*args, **kwargs)
-        instance.users.add(self.user)
-        return instance
+class TaskListUpdateForm(TaskListFormMixin, forms.ModelForm):
+    """ModelForm to update an instance of the ``TaskList`` model."""
+    class Meta:
+        model = TaskList
+        fields = ('title', 'users')
--- task_list/tests/forms_tests.py
+++ task_list/tests/forms_tests.py
@@ -3,8 +3,9 @@ from django.test import TestCase
 
 from django_libs.tests.factories import UserFactory
 
-from ..forms import TaskListCreateForm
+from ..forms import TaskListCreateForm, TaskListUpdateForm
 from ..models import TaskList
+from .factories import TaskListFactory
 
 
 class TaskListCreateFormTestCase(TestCase):
@@ -30,5 +31,40 @@ class TaskListCreateFormTestCase(TestCase):
             'After save, the user should be assigned to the list.'))
 
         form = TaskListCreateForm(data={}, user=self.user)
+        self.assertFalse(form.is_valid(), msg=(
+            'Without correct data, the form should not be valid.'))
+
+
+class TaskListUpdateFormTestCase(TestCase):
+    """Test for the ``TaskListUpdateForm`` form class."""
+    longMessage = True
+
+    def setUp(self):
+        self.user = UserFactory()
+        self.task_list = TaskListFactory()
+        self.task_list.users.add(self.user)
+        self.other_user = UserFactory()
+        self.valid_data = {
+            'title': 'task list title',
+            'users': [self.other_user.pk],
+        }
+
+    def test_form_validates_and_saves(self):
+        form = TaskListUpdateForm(data=self.valid_data, user=self.user,
+                                  instance=self.task_list)
+        self.assertTrue(form.is_valid(), msg=(
+            'With correct data, the form should be valid.'))
+
+        instance = form.save()
+        self.assertEqual(TaskList.objects.all().count(), 1, msg=(
+            'After save is called, there should be one task list in the db.'))
+
+        self.assertEqual(self.task_list.users.count(), 2, msg=(
+            'There should be two users assigned.'))
+
+        self.assertEqual(instance.users.all()[0], self.user, msg=(
+            'After save, the user should be assigned to the list.'))
+
+        form = TaskListUpdateForm(data={}, user=self.user)
         self.assertFalse(form.is_valid(), msg=(
             'Without correct data, the form should not be valid.'))
